909d9886565667d32fc2b4def5384f78a0f984a0
Andrzej <andstr2@st.amu.edu.pl>
2021-07-23 22:15:19 +0200

    It is possible to play one game

--- bin/main.py
+++ bin/main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame import *
 import sys
 import socket
+from threading import Thread
 
 import common.common as common
 import bin.classes.buttonClass as button
@@ -183,11 +184,23 @@ def CheckForWin(state_table, who):
 
 
 def WinText(text, game_display):
-    font = pygame.font.SysFont("Comic Sans MS", 60)
-    text = font.render(text, True, common.color_green, common.color_black)
-    textRect = text.get_rect()
-    textRect.center = (common.window_size_x // 2, common.window_size_y // 2)
-    game_display.blit(text, textRect)
+    my_font = pygame.font.SysFont("Comic Sans MS", 60)
+    text = my_font.render(text, True, common.color_green, common.color_black)
+    text_rect = text.get_rect()
+    text_rect.center = (common.window_size_x // 2, common.window_size_y // 2)
+    game_display.blit(text, text_rect)
+
+
+def socket_accept(server_socket, connection_and_client_address_list, create_new_server_window):
+    # Accept connection
+    connection_and_client_address_list[0], connection_and_client_address_list[1] = server_socket.accept()
+
+    create_new_server_window[0] = False
+    print("Connection accept successfully")
+
+
+def ReceiveData(from_who, received_data):
+    received_data[0] = int(from_who.recv(1024).decode())
 
 
 def main():
@@ -214,25 +227,31 @@ def main():
     # Init variables
     first_window_show = True
     join_to_server_window = False
-    create_new_server_window = False
+    create_new_server_window = [False]
     game_window_client = False
     game_window_server = False
-    text = None
+    # text = None
     is_server_created = False
-    server_socket = None
+    # server_socket = None
     client_socket = None
     is_server_turn = True
-    connection = None
+    # connection = None
+    is_first_time = True
+    connection_and_client_address_list = [None, None]   # 0 - connection, 1 - client address
+    received_data = [None]  # Stores data from threads
+    is_data_receiving = False
+    receive_thread = True
 
     text_input = textInput.TextInput(font_size=20)
     my_font = pygame.font.SysFont('Comic Sans MS', 15)
     text_server_ip = my_font.render('Server IP:', False, common.color_black)
-    text_your_server_ip = my_font.render(f'Your server IP: 127.0.0.1:{common.port}', False, common.color_black)  # ToDo change IP
+    # ToDo change IP to not local
+    text_your_server_ip = my_font.render(f'Your server IP: 127.0.0.1:{common.port}', False, common.color_black)
     text_waiting_for_another_player = my_font.render('Waiting for another player...', False, common.color_black)
 
     # Main loop
     while True:
-        # First window
+        # First window (create or join to server)
         if first_window_show:
             game_display.fill(common.color_light_gray)
             button_join_to_server = button.button(game_display, (common.window_size_x / 2 - 75, 75), "Join to server",
@@ -240,18 +259,18 @@ def main():
             button_create_new_server = button.button(game_display, (common.window_size_x / 2 - 100, 150),
                                                      "Create new server", 30, common.color_black, common.color_gray)
 
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
+            for my_event in pygame.event.get():
+                if my_event.type == pygame.MOUSEBUTTONDOWN:
                     if button_join_to_server.collidepoint(pygame.mouse.get_pos()):
                         first_window_show = False
                         join_to_server_window = True
 
                     if button_create_new_server.collidepoint(pygame.mouse.get_pos()):
                         first_window_show = False
-                        create_new_server_window = True
+                        create_new_server_window[0] = True
 
                 # Proper exit
-                if event.type == QUIT:
+                if my_event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
@@ -266,14 +285,14 @@ def main():
             # Get all events and store in variable "events" fro use in two places
             events = pygame.event.get()
 
-            for event in events:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+            for my_event in events:
+                if my_event.type == pygame.KEYDOWN:
+                    if my_event.key == pygame.K_ESCAPE:
                         join_to_server_window = False
                         first_window_show = True
 
                 # Proper exit
-                if event.type == QUIT:
+                if my_event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
@@ -285,7 +304,7 @@ def main():
                 join_to_server_window = False
 
         # Create new server window
-        elif create_new_server_window:
+        elif create_new_server_window[0]:
             game_display.fill(common.color_light_gray)
             # Show text
             game_display.blit(text_your_server_ip, (10, 0))
@@ -299,32 +318,39 @@ def main():
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket.bind(('127.0.0.1', common.port))
                 server_socket.listen(1)
-                connection, client_address = server_socket.accept()     # ToDo fix not responding error
+                # Use threading to connect
+                thread = Thread(target=socket_accept, args=(server_socket, connection_and_client_address_list, create_new_server_window))
+                thread.start()
+
                 is_server_created = True
                 game_window_server = True
-                create_new_server_window = False
 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        create_new_server_window = False
+            for my_event in pygame.event.get():
+                if my_event.type == pygame.KEYDOWN:
+                    if my_event.key == pygame.K_ESCAPE:
+                        create_new_server_window[0] = False
                         first_window_show = True
 
                 # Proper exit
-                if event.type == QUIT:
+                if my_event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
         elif game_window_server:
-            FillWindow(game_display)
+            if is_first_time:
+                FillWindow(game_display)
+                pygame.display.update()
+                is_first_time = False
 
             if is_server_turn:
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 1:
+                for my_event in pygame.event.get():
+                    # Is not the end of the game
+                    if my_event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 1:
                         mouse_poz = pygame.mouse.get_pos()
                         rectangle_number = WhichRectangle(mouse_poz)
                         rectangle_number_byte = bytes(str(rectangle_number), "utf-8")
-                        connection.sendall(rectangle_number_byte)
+                        # noinspection PyUnresolvedReferences
+                        connection_and_client_address_list[0].sendall(rectangle_number_byte)
                         Game(
                             game_display,
                             state_table,
@@ -333,86 +359,112 @@ def main():
                             list_of_rectangles,
                             is_end
                         )
-                    elif event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 2:
+
+                        is_server_turn = False
+                    # The end of the game (click on screen and game start again)
+                    elif my_event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 2:
                         # Fill the window with patterns
                         FillWindow(game_display)
 
                         # Init variables
                         state_table = [None for _ in range(9)]  # cross / zero
-                        rectangle_number = None
+                        # rectangle_number = None
                         who_is_next = ["cross"]  # cross / zero
                         is_end = [1]  # 1 - not the end, 2 - the end
+
+                        is_server_turn = False
 
                         pygame.display.update()
 
                     # Proper exit
-                    if event.type == QUIT:
+                    if my_event.type == QUIT:
                         pygame.quit()
                         sys.exit()
+
             else:
-                rectangle_number = int(connection.recv(1024).decode())
-                Game(
-                    game_display,
-                    state_table,
-                    rectangle_number,
-                    who_is_next,
-                    list_of_rectangles,
-                    is_end
-                )
+                if not is_data_receiving:
+                    receive_thread = Thread(target=ReceiveData, args=(connection_and_client_address_list[0], received_data))
+                    receive_thread.start()
 
-        elif game_window_client:
-            FillWindow(game_display)
-            pygame.display.update()
+                    is_data_receiving = True
 
-            if not is_server_turn:
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 1:
-                        mouse_poz = pygame.mouse.get_pos()
-                        rectangle_number = WhichRectangle(mouse_poz)
-                        rectangle_number_byte = bytes(str(rectangle_number), "utf-8")
-                        connection.sendall(rectangle_number_byte)
-                        Game(
-                            game_display,
-                            state_table,
-                            rectangle_number,
-                            who_is_next,
-                            list_of_rectangles,
-                            is_end
-                        )
-                    elif event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 2:
-                        # Fill the window with patterns
-                        FillWindow(game_display)
-
-                        # Init variables
-                        state_table = [None for _ in range(9)]  # cross / zero
-                        rectangle_number = None
-                        who_is_next = ["cross"]  # cross / zero
-                        is_end = [1]  # 1 - not the end, 2 - the end
-
-                        pygame.display.update()
-
-                    # Proper exit
-                    if event.type == QUIT:
-                        pygame.quit()
-                        sys.exit()
-            else:
-                rectangle_number = None
-                try:
-                    rectangle_number = connection.recv(1024).decode()
-                    print(f"rectangle_number: {rectangle_number}")
-
+                # rectangle_number = int(connection_and_client_address_list[0].recv(1024).decode())
+                if not receive_thread.is_alive():
                     Game(
                         game_display,
                         state_table,
-                        rectangle_number,
+                        received_data[0],
                         who_is_next,
                         list_of_rectangles,
                         is_end
                     )
 
+                    is_data_receiving = False
                     is_server_turn = True
-                except Exception:
-                    pass
+
+        elif game_window_client:
+            if is_first_time:
+                FillWindow(game_display)
+                pygame.display.update()
+                is_first_time = False
+
+            if not is_server_turn:
+                # Is not the end of the game
+                for my_event in pygame.event.get():
+                    if my_event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 1:
+                        mouse_poz = pygame.mouse.get_pos()
+                        rectangle_number = WhichRectangle(mouse_poz)
+                        rectangle_number_byte = bytes(str(rectangle_number), "utf-8")
+                        client_socket.sendall(rectangle_number_byte)
+                        Game(
+                            game_display,
+                            state_table,
+                            rectangle_number,
+                            who_is_next,
+                            list_of_rectangles,
+                            is_end
+                        )
+
+                        is_server_turn = True
+
+                    # The end of the game (click on screen and game start again)
+                    elif my_event.type == pygame.MOUSEBUTTONDOWN and is_end[0] == 2:
+                        # Fill the window with patterns
+                        FillWindow(game_display)
+
+                        # Init variables
+                        state_table = [None for _ in range(9)]  # cross / zero
+                        # rectangle_number = None
+                        who_is_next = ["cross"]  # cross / zero
+                        is_end = [1]  # 1 - not the end, 2 - the end
+
+                        is_server_turn = True
+
+                        pygame.display.update()
+
+                    # Proper exit
+                    if my_event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+            else:
+                if not is_data_receiving:
+                    receive_thread = Thread(target=ReceiveData, args=(client_socket, received_data))
+                    receive_thread.start()
+
+                    is_data_receiving = True
+
+                if not receive_thread.is_alive():
+                    Game(
+                        game_display,
+                        state_table,
+                        received_data[0],
+                        who_is_next,
+                        list_of_rectangles,
+                        is_end
+                    )
+
+                    is_data_receiving = False
+                    is_server_turn = False
 
         pygame.display.update()
         fps_clock.tick(60)
